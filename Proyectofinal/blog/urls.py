6e388c904466c3e8974febe6db78f6aee0729234
Keila Fulquin <keilafulquin@gmail.com>
2022-10-11 15:05:58 -0300

    actualizacion

---
+++ Proyectofinal/blog/urls.py
@@ -0,0 +1,12 @@
+
+from django import views
+from django.urls import path
+from blog.views import mostrar_inicio, autores, articulos, seccion, contacto
+
+urlpatterns = [
+    path('inicio/', mostrar_inicio, name = "Inicio"),
+    path('autor/', autores, name = "Autores"),
+    path('articulo/', articulos, name = "Articulos"),
+    path('seccion/', seccion, name = "Seccion"),
+    path('contacto/', contacto, name = "Contacto"),
+]
--- Proyectofinal/blog/views.py
+++ Proyectofinal/blog/views.py
@@ -1,3 +1,31 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from blog.models import Autor
+
 # Create your views here.
+
+
+def mostrar_inicio(request):
+    return render(request, 'blog/inicio.html')
+
+
+def autores(request):
+    if request.method != "POST":
+        return render(request, "blog/autores.html")
+
+    autor = Autor(nombre=request.POST["nombre"], apellido=request.POST["apellido"], edad=request.POST["edad"], profesion=request.POST["profesion"])
+    autor.save()
+    return render(request, 'blog/autores.html')
+
+
+def articulos(request):
+    return render(request, 'blog/articulos.html')
+
+
+def seccion(request):
+    return render(request, 'blog/seccion.html')
+
+
+def contacto(request):
+    return render(request, 'blog/contacto.html')
